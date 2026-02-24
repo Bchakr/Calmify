813c331c96785258feace9599f2725ee2bbd81ca
Nakshatra Gupta <ng1308@iastate.edu>
2026-02-24 02:16:28 -0600

    Completed signup, home page with profile button, profile deletion with Mockoon CRUD

---
+++ Frontend/Nakshatra/app/src/main/java/com/example/antianxietyapp/ApiConstants.java
@@ -0,0 +1,7 @@
+package com.example.antianxietyapp;
+
+public class ApiConstants {
+
+    public static final String BASE_URL = "http://10.0.2.2:1308";
+    public static final String USERS = BASE_URL + "/users";
+}
